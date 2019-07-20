e71e6cf1eaa0a911e8ff4b837a6f618f21594e9d
Jellene Khoh <jellene4eva@gmail.com>
2019-07-20 14:43:58 +0300

    Section 2.1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,73 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
+const circleR = 80;
+const circleDasharray = 2 * Math.PI * circleR;
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss']
 })
 export class HomePage {
+    time: BehaviorSubject<string> = new BehaviorSubject('00:00');
+    percent: BehaviorSubject<number> = new BehaviorSubject(100);
+    
+    timer: number; // in seconds
+    interval;
 
-  constructor() {}
+    startDuration = 1;
 
+    circleR = circleR;
+    circleDasharray = circleDasharray;
+
+    state: 'start' | 'stop' = 'stop';
+
+    constructor() {}
+
+    startTimer(duration: number) {
+        this.state = 'start';
+        clearInterval(this.interval);
+        this.timer = duration * 5;
+        this.updateTimeValue();
+        this.interval = setInterval( () => {
+            this.updateTimeValue();
+        }, 1000);
+    }
+
+    stopTimer() {
+        clearInterval(this.interval);
+        this.time.next('00:00');
+        this.state = 'stop';
+    }
+
+
+    percentageOffset(percent) {
+        const percentFloat = percent / 100;
+        return circleDasharray * (1 - percentFloat);
+    }
+
+    swapDuration() {
+        this.startDuration = this.startDuration === 1 ? 0.5 : 1;
+    }
+
+    updateTimeValue() {
+        let minutes: any = this.timer / 60;
+        let seconds: any = this.timer % 60;
+
+        minutes = String('0' + Math.floor(minutes)).slice(-2);
+        seconds = String('0' + Math.floor(seconds)).slice(-2);
+
+        const text = minutes + ':' + seconds;
+        this.time.next(text);
+
+        const totalTime = this.startDuration * 5;
+        const percentage = ((totalTime - this.timer) / totalTime) * 100;
+        this.percent.next(percentage);
+
+        --this.timer;
+        if (this.timer < -1) {
+            this.swapDuration();
+            this.startTimer(this.startDuration);
+        }
+    }
 }
